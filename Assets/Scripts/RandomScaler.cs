32f3f7084c2a5205c25c3501645585e062c46859
marcosecchi <loziosecchi@gmail.com>
2026-02-19 10:15:12 +0100

    Add random scaler.

---
+++ Assets/Scripts/RandomScaler.cs
@@ -0,0 +1,17 @@
+using UnityEngine;
+
+public class RandomScaler : MonoBehaviour
+{
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        var scale = Random.Range(0.85f, 1.5f);
+        transform.localScale = new Vector3(scale, scale, scale);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
