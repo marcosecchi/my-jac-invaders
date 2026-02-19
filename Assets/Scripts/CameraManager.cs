ae51390d1f6cb89719ec08db98dac971907bb96e
marcosecchi <loziosecchi@gmail.com>
2026-02-19 12:32:29 +0100

    Add camera switch.

--- Assets/Scripts/CameraManager.cs
+++ Assets/Scripts/CameraManager.cs
@@ -2,26 +2,40 @@ using UnityEngine;
 
 public class CameraManager : MonoBehaviour
 {
-    public Transform target;
+    public Transform target1;
+    public Transform target2;
     public Transform ship;
+
+    Transform actualTarget;
 
     [Range(0f, 100f)]
     public float followSpeed = 1.0f;
 
     void Start()
     {
-        
+        actualTarget = target1;
     }
 
     void Update()
     {
         // Fa schifo   transform.position = target.position;
 
+        if(Input.GetKeyDown(KeyCode.R))
+        {
+            if(actualTarget == target1)
+            {
+                actualTarget = target2;
+            }
+            else
+            {
+                actualTarget = target1;
+            }
+        }
         var newPosition = Vector3.Lerp(
-            transform.position, target.position, Time.deltaTime * followSpeed);
+            transform.position, actualTarget.position, Time.deltaTime * followSpeed);
         transform.position = newPosition;
 
         // Decommentare se si vuole che la camera punti al target
-        // transform.LookAt(ship.position);
+        transform.LookAt(ship.position);
     }
 }
