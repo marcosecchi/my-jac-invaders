128f922eeb1f4272c5b214e45f4985f5c2cde2de
marcosecchi <loziosecchi@gmail.com>
2026-02-19 09:59:20 +0100

    Update prefabs.

---
+++ Assets/Scripts/RandomDestroyer.cs
@@ -0,0 +1,22 @@
+using UnityEngine;
+
+public class RandomDestroyer : MonoBehaviour
+{
+    [Range(0, 1.0f)]
+    public float DestroyPercentage = .5f;
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        if(Random.Range(0, 1.0f) < DestroyPercentage)
+        {
+            Destroy(gameObject);
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
