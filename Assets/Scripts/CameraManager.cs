96f8349ccd59682e2d0f9b8e6754c5e815ae6d8e
marcosecchi <loziosecchi@gmail.com>
2026-02-19 12:18:11 +0100

    Improve camera management.

--- Assets/Scripts/CameraManager.cs
+++ Assets/Scripts/CameraManager.cs
@@ -3,6 +3,10 @@ using UnityEngine;
 public class CameraManager : MonoBehaviour
 {
     public Transform target;
+    public Transform ship;
+
+    [Range(0f, 100f)]
+    public float followSpeed = 1.0f;
 
     void Start()
     {
@@ -13,7 +17,11 @@ public class CameraManager : MonoBehaviour
     {
         // Fa schifo   transform.position = target.position;
 
-        var newPosition = Vector3.Lerp(transform.position, target.position, Time.deltaTime * 3);
+        var newPosition = Vector3.Lerp(
+            transform.position, target.position, Time.deltaTime * followSpeed);
         transform.position = newPosition;
+
+        // Decommentare se si vuole che la camera punti al target
+        // transform.LookAt(ship.position);
     }
 }
